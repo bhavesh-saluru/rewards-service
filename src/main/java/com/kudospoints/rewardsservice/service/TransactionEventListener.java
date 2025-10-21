c7300e265b978cf19379f58128ee32dfefe6f799
Bhavesh Saluru <bhaveshsaluru9@gmail.com>
2025-10-21 19:46:57 +0530

    Listener expects TransactionEvent Object instead of String
    - Message Converter takes care of converting JSON to TransactionEvent Object
    - If the payload is not in sync with our TransactionEvent DTO, message will not be processed and sent to DLQ (Dead Letter Queue) used for debugging, monitoring, or retrying failed messages.

--- src/main/java/com/kudospoints/rewardsservice/service/TransactionEventListener.java
+++ src/main/java/com/kudospoints/rewardsservice/service/TransactionEventListener.java
@@ -1,5 +1,6 @@
 package com.kudospoints.rewardsservice.service;
 
+import com.kudospoints.rewardsservice.dto.TransactionEvent;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
@@ -7,7 +8,10 @@ import org.springframework.stereotype.Service;
 public class TransactionEventListener {
 
     @RabbitListener(queues = "transactions.queue")
-    public void handleTransactionEvent(String message) {
-        System.out.println("Received event for member: " + message);
+    public void handleTransactionEvent(TransactionEvent event) {
+        System.out.println("Received event for member: " + event.getMemberId());
+        System.out.println("Amount: " + event.getAmount());
+        System.out.println("Payment method type: " + event.getPaymentMethod());
+        System.out.println("--------------------------");
     }
 }
